cf2838dff3cb872531dc81ac2f7aafcd217bb4ab
HUZAIFA <karimjawwad09@gmail.com>
2025-04-17 16:33:55 +0500

    URL changed

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = "https://ai-car-widget-backend.vercel.app";
+export const SERVER_URL = "https://www.widget.moving7.com";
 // export const SERVER_URL = "http://127.0.0.1:8000";
 // export const SERVER_URL = "http://localhost:8084";
