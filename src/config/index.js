3934f11a5ad1920c65c53b689ae3562f0578804d
HUZAIFA <karimjawwad09@gmail.com>
2025-05-02 11:28:03 +0500

    UI updated

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = "https://www.widget.moving7.com";
+// export const SERVER_URL = "https://www.widget.moving7.com";
 // export const SERVER_URL = "http://127.0.0.1:8000";
-// export const SERVER_URL = "http://localhost:8084";
+export const SERVER_URL = "http://localhost:8000";
