9098c8fe8e138f02fa40687006d750095bfafbc8
Syed Jawwad Kareem Naqvi <139786195+jawwadkareem@users.noreply.github.com>
2025-09-29 12:12:06 +0500

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 // export const SERVER_URL = "https://www.widget.moving7.com";
 // export const SERVER_URL = "http://127.0.0.1:8000";
-export const SERVER_URL = "http://lmg.justjdmcars.com.au";
+export const SERVER_URL = "https://lmg.justjdmcars.com.au";
 // export const SERVER_URL = "https://phm.justjdmcars.com.au";
